f4d9213af8ccdaa9bf78fd83a0614bec07796e75
Pradeep K R <pradeep.ravibaskar@gmail.com>
2023-08-23 23:58:26 +0530

    fix

--- src/app/routes/propertyOwnerRoutes.ts
+++ src/app/routes/propertyOwnerRoutes.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 
 // Define Property Owner routes
 
-router.post('/catalogue', isAuthenticated, isAuthorized({ hasRole: ['property_owner','admin'] }), postTravelCatalogue);
+router.post('/catalogue', isAuthenticated, isAuthorized({ hasRole: ['admin'], allowSameUser: true }), postTravelCatalogue);
 
 export default router;
